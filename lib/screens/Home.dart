db65e96ff1da516a0a932717b00ad81eb6076956
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-13 01:30:55 +0530

    services added

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/services/GetWeatherDetails.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+  @override
+  void initState() {
+    super.initState();
+    getWeatherdetails('https://hg-weather-application.herokuapp.com/weather?address=delhi');
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      
     );
   }
 }
--- lib/services/GetWeatherDetails.dart
+++ lib/services/GetWeatherDetails.dart
@@ -1,4 +1,17 @@
-//
-// Future getWeatherDetails(String url) async{
-//   const url = url;
-// }
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future getWeatherdetails(String url) async{
+  http.Response response = await http.get(url);
+  print(response.statusCode);
+  if(response.statusCode == 200){
+    try{
+      var jsondata = json.decode(response.body);
+      print(jsondata['name']);
+
+    }catch(e){
+
+    }
+  }
+}
